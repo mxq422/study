cd2ee0053c0f5040dac1ab7281363070122339bd
mxq422 <maxianqiang422@qq.com>
2020-01-06 09:57:38 +0800

    1

--- 文件处理.py
+++ 文件处理.py
@@ -3,6 +3,7 @@ import os
 import xlrd
 import pandas as pd
 import pprint
+import numpy as np
 
 def listdir(path,list_name=[]):#列举指定路径内所有含路径文件名称（包含二级及以上路径）
     for file in os.listdir(path):
@@ -12,13 +13,30 @@ def listdir(path,list_name=[]):#列举指定路径内所有含路径文件名称
         else:
             list_name.append(file_path)
     return list_name
+
+def content(path):#提取excel内容并形成字典
+    book=xlrd.open_workbook(path)
+    sheet=book.sheet_by_name("Total") 
+    . 
+    data={}
+    for i in range(7,sheet.nrows):
+        row=sheet.row_values(i)
+        #        data.append(row)
+        #    return data
+        #    row=sheet.row_values(i)
+        #country=row[0]
+        #data[country]={"1月":row[1],"2月":row[2],"3月":row[3]}
+        data=np.array(row)
+    return data
 path="D:/111"
 list_name=listdir(path)
-book=xlrd.open_workbook(list_name[0])
-sheet=book.sheet_by_name("Total")
-data={}
-#for i in range(7,sheet.nrows):
-#    row=sheet.row_values(i)
-#    country=row[0]
-#   data[country]={"1月":[],"2月":[],"3月":[]}
-#pprint.pprint(data)
+dict_1={}
+dict_2={}
+list_1=[]
+
+for i in list_name:
+    dict_1[i]=content(i)
+    print(content(i))
+    list_1.append(list(dict_1.values()))
+pprint.pprint(list_1)
+    
