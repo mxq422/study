4a6f39fd8d4351fc3523fb04628be501a8d78d2e
mxq422 <maxianqiang422@qq.com>
2020-03-21 13:23:21 +0800

    1

--- 神经网络.py
+++ 神经网络.py
@@ -1,3 +1,11 @@
+
+# coding: utf-8
+import sys, os
+sys.path.append(os.pardir)  # 为了导入父目录的文件而进行的设定
+import numpy as np
+from dataset.mnist import load_mnist
+from PIL import Image
+
 import numpy as np
 import matplotlib.pyplot as plt
 def sigmoid(x):
