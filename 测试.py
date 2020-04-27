ce20cba19f0e36231ad0245c7e4ae2a2114ab695
mxq422 <maxianqiang422@qq.com>
2020-04-27 22:58:28 +0800

    1

---
+++ 测试.py
@@ -0,0 +1,32 @@
+# def twoSum (self, nums, target):
+#         #for   i in range(1,len(nums)):
+#     for i in range(1,len(nums)),j=j in range(1,len(nums)):
+#         print(i,j)
+#         if  i==j:
+#             pass
+#         elif  i != j:
+#             if nums[j]+nums[j]==target:
+#             print(i,j)
+#                 break
+#             else:
+#                 pass
+#         else:
+#             pass
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        result = list()
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if target == nums[i] + nums[j]:
+                    result.append(i)
+                    result.append(j)
+        return result
+nums=[2,7,12,15]
+target=9
+twoSum(nums,target)
